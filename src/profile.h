084eea4e70a7835ebd700a37021e74a8c7f7ae8d
Srinjoy Majumdar <srinjoy.majumdar.iiit@gmail.com>
2026-08-16 15:01:58 +0530

    Add email field to Profile and include it in rendered output

--- src/profile.h
+++ src/profile.h
@@ -1,14 +1,19 @@
 #ifndef PROFILE_H
 #define PROFILE_H
+
 #include <string>
 
 class Profile {
 public:
     Profile(const std::string& owner);
     void setBio(const std::string& bio);
+    void setEmail(const std::string& email);
     std::string render() const;
+
 private:
     std::string owner;
     std::string bio;
+    std::string email;
 };
+
 #endif
