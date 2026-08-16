cee44efa2fb71b5e6a8ed2454dd476ede559c770
Srinjoy Majumdar <srinjoy.majumdar.iiit@gmail.com>
2026-08-16 14:33:13 +0530

    Add Settings interface for theme and notification preferences

---
+++ src/settings.h
@@ -0,0 +1,15 @@
+#ifndef SETTINGS_H
+#define SETTINGS_H
+#include <string>
+
+class Settings {
+public:
+    Settings();
+    void setTheme(const std::string& theme);
+    void toggleNotifications();
+    std::string summary() const;
+private:
+    std::string theme;
+    bool notifications;
+};
+#endif
